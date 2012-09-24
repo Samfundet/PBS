06f323469728667d4850285524548afa556618e5
Jonas Amundsen <JonasBA@Gmail.com>
2012-09-24 19:37:15 +0200

    Initial commit, containing a basic rails application with some of the gems we're going to use.

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,5 @@
+class SiteController < ApplicationController
+  def index
+    @my_var = 'OMGz0r!!!1'
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PbsLocalhostNo::Application.config.secret_token = '94abf23c70ca8716d69c3b112b42940cb678b834cb62bcc7616ad59fe5f88bd4aecb265544157e5ed48c323f42a4d726a83cce4b85aaffc6960b98b72ebe244d'
