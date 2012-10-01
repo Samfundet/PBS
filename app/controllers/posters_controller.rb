59bb34bed2fdb53e3615a273004700b1383b2d83
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-01 22:58:01 +0200

    Making a posters_controller

---
+++ app/controllers/posters_controller.rb
@@ -0,0 +1,23 @@
+class PostersController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def cancel
+  end
+
+  def take
+  end
+
+  def archive
+  end
+
+  def delete
+  end
+end
