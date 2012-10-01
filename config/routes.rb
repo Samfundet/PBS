be6e06429cc28a1dc9862e89512c644eedaa3a40
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-01 23:24:43 +0200

    Adding posters to the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,8 @@ PbsLocalhostNo::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
   resources :dimensions
-  root :to => 'dimensions#index'
+  resources :posters
+  root :to => 'posters#index'
 
   # See how all your routes lay out with "rake routes"
 
