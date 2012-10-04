a8a5cc67572bd72167c5633bf487c2b5e3c2f257
scbasma <sondre1504@gmail.com>
2012-10-04 18:27:39 +0200

    Endret på create slik at vi får lagret plakaten.

--- app/controllers/posters_controller.rb
+++ app/controllers/posters_controller.rb
@@ -1,9 +1,23 @@
 class PostersController < ApplicationController
 
   def index
+    @posters=Posters.all
   end
 
   def create
+    @poster = Poster.new(params[:poster])
+    if @poster.save
+      flash[:success] = "Plakaten er laget."
+    else
+      flash[:error] = "Vennligst fyll ut alle felter."
+      render :action => 'new'
+    end
+  end
+
+  def update
+  end
+
+  def show
   end
 
   def new
