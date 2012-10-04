8d6fb72ae300ddccbcdc27a25ee5b0a38fb75519
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-04 22:56:42 +0200

    Inserting an archived field into the migrate_posters.

--- db/migrate/20120924182748_create_posters.rb
+++ db/migrate/20120924182748_create_posters.rb
@@ -6,8 +6,10 @@ class CreatePosters < ActiveRecord::Migration
       t.text :description
       t.boolean :flyers
       t.boolean :banner
+      t.boolean :archived, :default =>false
       t.datetime :send_to_press
       t.references :dimension
+
       #t.has_one :event, :class_name => "Event"
       #t.has_one :orderer, :class_name => "User"
       #t.has_one :responsible, :class_name => "User"
