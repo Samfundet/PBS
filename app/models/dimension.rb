dd9e2ed75eff585d7795c051a96de2c7ad1d7e32
Morten Olsen Lysgaard <morten@lysgaard.no>
2012-10-08 19:47:55 +0200

    linket posters og dimesjoner

--- app/models/dimension.rb
+++ app/models/dimension.rb
@@ -1,2 +1,6 @@
 class Dimension < ActiveRecord::Base
+  has_many :poster
+  def to_s
+    text
+  end
 end
--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,4 +1,4 @@
 class Poster < ActiveRecord::Base
-  attr_accessible :name, :poster_text, :description, :flyers, :banner, :archived, :send_to_press
-	has_one :dimension
+  attr_accessible :name, :poster_text, :description, :flyers, :banner, :archived, :send_to_press, :dimension_id
+	belongs_to :dimension
 end
