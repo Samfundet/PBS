06d13a37989478b33930f41d3c7b3431d24d7373
scbasma <sondre1504@gmail.com>
2012-10-15 20:19:01 +0200

    Added status hash for posters.

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -4,4 +4,8 @@ class Poster < ActiveRecord::Base
 	belongs_to :dimension
   belongs_to :responsible, :class_name => 'Member', :foreign_key => :responsible_id
   belongs_to :orderer, :class_name => 'Member', :foreign_key => :orderer_id
+
+  @@POSTER_STATUSES = {:active => 'Aktiv', :canceled => 'Avbestilt', :archived => 'Arkivert', nil => 'Ikke satt'}
+  validates_inclusion_of :poster_status, :in => @@POSTER_STATUSES.keys, :message => "Invalid poster status"
+
 end
