f4e3f882b38a8be4b0208d6e43a164ec06083b39
scbasma <sondre1504@gmail.com>
2012-11-12 23:08:06 +0100

    Autocomplete for the time of the chosen event in form.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # -*- encoding : utf-8 -*-
-module ApplicationHelper
+module ApplicationHelper 
+
+  def custom_event_options
+    events = Event.all.map do |e|
+      [e.name,e, {:event_time => e.time.to_s}]
+  	end
+    return options_for_select(events)
+  end
+
+
 end
