85bfbcbe4db2d3e1a82b592559e20b916543851a
Rune Holmgren <Raane.Holm@gmail.com>
2012-11-15 20:07:45 +0100

    Adding CSS and som HTML styling the asserts in the show view in a nice grid view.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,9 +5,7 @@ class Asset < ActiveRecord::Base
 
   has_attached_file :image,
     :styles => {
-        :thumb => '150x150#',
-        :medium => '300x300>',
-        :large => '600x600>'
+        :thumb => '150x150#'
     }
 
 end
