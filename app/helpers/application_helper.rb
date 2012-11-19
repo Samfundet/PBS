6e1bd5bbd3da5e6b26e77f73a170c717e8c16444
Rune Holmgren <Raane.Holm@gmail.com>
2012-11-19 22:29:31 +0100

    Changing the helper for the event so that event name will be shown correctly.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 
   def custom_event_options
     events = Event.all.map do |e|
-      [e.name,e, {:event_time => e.time.strftime("%d.%m.%Y  %H:%M")}]
+      [e.name,e.id, {:event_time => e.time.strftime("%d.%m.%Y  %H:%M")}]
   	end
     return options_for_select(events)
   end
